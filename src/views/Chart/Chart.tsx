1030409980d76ceeedd9daa95041a4f916621f50
vgoer <hi_goer@163.com>
2023-06-16 18:11:38 +0800

    charts:echarts图表

--- src/views/Chart/Chart.tsx
+++ src/views/Chart/Chart.tsx
@@ -1,9 +1,38 @@
 import React from "react"
+import ReactEcharts from "echarts-for-react"
+import echarts from "echarts";
 
 const Charts: React.FC = () => {
+    
+    const getOption = () => {
+
+        let option = {
+            xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            },
+            yAxis: {
+            type: 'value'
+            },
+            series: [
+            {
+                data: [820, 932, 901, 934, 1290, 1330, 1320],
+                type: 'line',
+                smooth: true
+            }
+            ]
+        }
+        return option
+    }
+
     return (
-        <div>hello</div>
+
+        <div className="charts">
+            <h3>hello</h3>
+            <ReactEcharts style={{width:'700px',height:'500px'}} option={getOption()} />
+        </div>
     )
+
 }
 
 
